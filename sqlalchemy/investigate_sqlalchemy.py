419ac817cf5c5847025815fdcedbf7b8dc11b7a8
John McCloskey <jmccloskey2@wisc.edu>
2022-08-15 12:34:11 -0700

    adjust reprs

--- sqlalchemy/investigate_sqlalchemy.py
+++ sqlalchemy/investigate_sqlalchemy.py
@@ -3,11 +3,25 @@ https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html
 https://docs.sqlalchemy.org/en/14/orm/tutorial.html
 https://docs.sqlalchemy.org/en/14/core/engines.html#sqlite
 """
+import typing as tp
 
-from sqlalchemy import Column, ForeignKey, Integer, Table, create_engine
+from sqlalchemy import Column, ForeignKey, Integer, Table, create_engine, inspect
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.sql.schema import Column
 
-Base = declarative_base()
+
+class _Base:
+    @classmethod
+    def get_columns(cls) -> tp.Tuple[Column, ...]:
+        return tuple(inspect(cls).columns)
+
+    def __repr__(self) -> str:
+        items = ((c.name, getattr(self, c.name)) for c in self.get_columns())
+        inner = ", ".join(f"{k}={v}" for k, v in items)
+        return f"{self.__class__.__name__}({inner})"
+
+
+Base = declarative_base(cls=_Base)
 
 
 class Parent(Base):
@@ -15,18 +29,12 @@ class Parent(Base):
     id = Column(Integer, primary_key=True)
     children = relationship("Child")
 
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(id={self.id}, children={self.children})"
-
 
 class Child(Base):
     __tablename__ = "child"
     id = Column(Integer, primary_key=True)
     parent_id = Column(Integer, ForeignKey("parent.id"))
     parent = relationship("Parent", back_populates="children")
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(id={self.id}, parent_id={self.parent_id})"
 
 
 engine = create_engine("sqlite:///mydb.db", echo=True)
