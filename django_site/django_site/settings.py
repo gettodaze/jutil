ab770689ccf01ec55f824bf3e164fcf9eb23f1c9
John McCloskey <jmccloskey2@wisc.edu>
2022-08-16 14:51:47 -0700

    change settings

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from django_site import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x0#)qu)^qm+9%(9to%*($haj@u=k6^*vs_50!0$k-(&*dl7(r5"
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
