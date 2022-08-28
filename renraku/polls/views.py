89eead18b002bb05a9b46f8b2aeeced55f205ecc
gettodaze <jmccloskey2@wisc.edu>
2022-08-28 15:48:27 -0700

    follow along more tutorial

--- renraku/polls/views.py
+++ renraku/polls/views.py
@@ -1,8 +1,9 @@
 import django
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from renraku.polls.models import Question
+from django.urls import reverse
+from renraku.polls.models import Choice, Question
 
 
 # Create your views here.
@@ -25,3 +26,31 @@ def results(_: HttpRequest, question_id: int) -> HttpResponse:
 
 def vote(_: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse(f"question {question_id} voting.")
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
