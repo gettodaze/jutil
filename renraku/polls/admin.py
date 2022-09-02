0ad7b296089a64e6fa87f2d9bdf36fad3febefa7
gettodaze <jmccloskey2@wisc.edu>
2022-09-02 12:49:17 -0700

    add some admin

--- renraku/polls/admin.py
+++ renraku/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from renraku.polls.models import Choice, Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date")
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("question", "choice_text", "votes")
+
 
 # Register your models here.
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
