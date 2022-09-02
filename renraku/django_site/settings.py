261a82f1c59f60edb4275254500bce1c2048c38a
gettodaze <jmccloskey2@wisc.edu>
2022-09-02 14:00:59 -0700

    add rest framework config

--- renraku/django_site/settings.py
+++ renraku/django_site/settings.py
@@ -134,3 +134,11 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+    ]
+}
