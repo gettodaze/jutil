1d1190e4bf41baad5e33c2e2de3745351a726c7c
gettodaze <jmccloskey2@wisc.edu>
2022-09-02 12:56:27 -0700

    add todo config

--- renraku/django_site/settings.py
+++ renraku/django_site/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "renraku.polls.apps.PollsConfig",
+    "renraku.polls.apps.TodoConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
