a4534aeb1c19cc8f2ae7f2171ff8475180552f61
gettodaze <jmccloskey2@wisc.edu>
2022-09-02 12:59:56 -0700

    add model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class TodoTask(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(default="")
+    completed = models.BooleanField(default=False)
+
+
 # Create your models here.
