20161d4cc483da214ea5b1b6b3d7862a4965bda8
gettodaze <jmccloskey2@wisc.edu>
2022-09-02 15:51:20 -0700

    add more cli methods

--- renraku_cli/core/todo_task.py
+++ renraku_cli/core/todo_task.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import typing as tp
 
 import requests
@@ -6,20 +8,40 @@ from renraku_cli.core.config import User, task_user
 JSON = tp.Dict[str, tp.Any]
 BASE_URL = "http://127.0.0.1:8000/todo/api"
 
+TITLE = "title"
+DESCRIPTION = "description"
+COMPLETED = "completed"
+TASK_ID = "id"
+
+
+class Task(tp.NamedTuple):
+    title: str
+    description: str
+    completed: bool
+    task_id: int
+
+    @classmethod
+    def from_json(cls, json: dict) -> Task:
+        json["task_id"] = json.pop(TASK_ID)
+        return cls(**json)
+
 
 def _call_endpoint(endpoint: str, method: str = "get", **kwargs) -> JSON:
     url = BASE_URL + endpoint
+    print(f"{method.upper():<10} {url}\n{kwargs}\n")
     resp = requests.request(method, url, **kwargs)
     resp.raise_for_status()
     return resp.json()
 
 
-def get_task(task_id: int) -> JSON:
-    return _call_endpoint(f"/todos/{task_id}")
+def get_task(task_id: int) -> Task:
+    json = _call_endpoint(f"/todos/{task_id}")
+    return Task.from_json(json)
 
 
-def get_all_tasks() -> JSON:
-    return _call_endpoint("/todos")
+def get_all_tasks() -> tuple[Task, ...]:
+    json = _call_endpoint("/todos")
+    return tuple(Task.from_json(el) for el in json)
 
 
 def post_task(
@@ -27,11 +49,37 @@ def post_task(
     user: User = task_user,
     description: str = "",
     completed: bool = False,
-) -> JSON:
+) -> Task:
     data = dict(title=title, description=description, completed=completed)
-    return _call_endpoint(
+    json = _call_endpoint(
         "/todos/", method="post", data=data, auth=(user.username, user.password)
     )
+    return Task.from_json(json)
+
+
+def update_task(
+    task_id: int,
+    title: str | None = None,
+    description: str | None = None,
+    completed: bool | None = None,
+    user: User = task_user,
+) -> Task:
+    items = [(TITLE, title), (DESCRIPTION, description), (COMPLETED, completed)]
+    data = {k: v for k, v in items if v is not None}
+    if not data:
+        raise ValueError("Must specify one item")
+
+    # need to figure out how to make title optional in put requests
+    if title is None:
+        data[TITLE] = get_task(task_id).title
+
+    json = _call_endpoint(
+        f"/todos/{task_id}/",
+        method="put",
+        json=data,
+        auth=(user.username, user.password),
+    )
+    return Task.from_json(json)
 
 
 if __name__ == "__main__":
