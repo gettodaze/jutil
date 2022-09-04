ae2f2aa3786eec104dad61f2a62c0944dd15e99e
gettodaze <jmccloskey2@wisc.edu>
2022-09-04 07:19:27 -0700

    i dont know what im doing

--- renraku_react/src/App.js
+++ renraku_react/src/App.js
@@ -1,16 +1,68 @@
 import './App.css';
+import React from 'react';
+var ReactDOM = require('react-dom')
 
-async function TodoList() {
-  fetch('http://127.0.0.1:8001/todo/api/todos').then(
-    resp => console.log(resp)
-  )
+function formatTodo(todo) {
+  // note: we are adding a key prop here to allow react to uniquely identify each
+  // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+  return <><input type="checkbox" key={todo.id} checked={todo.completed}></input><label>{todo.description}</label></>
 }
 
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:8001/todo/api/todos')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>{items.map(formatTodo)}</ul>
+      );
+    }
+  }
+}
+
+const root = ReactDOM.client.createRoot(
+  document.getElementById('root')
+);
+
 function App() {
-  TodoList()
   return (
     <div className="App">
-      <p>Todos</p>
+      {root.render(TodoList)}
       <form>
         <label>
           Add Todo:
